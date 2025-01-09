ecb4fe04146c45ba67d4cb3977a3367e9e1455ac
Akhand <39897092+akhand96@users.noreply.github.com>
2025-01-09 17:57:08 +0530

    file-compression

---
+++ file-compression.py
@@ -0,0 +1,17 @@
+# importing the required libraries
+import gzip
+import shutil
+
+# function to compress a file using gzip
+def compressFile(inputFile, outputFile):
+    with open(inputFile, 'rb') as f_in:
+        with gzip.open(outputFile, 'wb') as f_out:
+            shutil.copyfileobj(f_in, f_out)
+
+# setting the input and  output path
+input_path = input("Enter the path of file to be compressed: ")
+output_path = 'compressed_output_file.pdf.gz' # replace the .pdf with your actual file format
+
+# calling the function to compress the file
+compressFile(input_path, output_path)
+
